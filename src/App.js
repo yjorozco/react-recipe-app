7c43a2db1bdc7a121e31b8895939d9f08e6189dc
Yamil Orozco <yamiloro@gmail.com>
2019-09-03 21:31:56 -0430

    fixing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 import React, { Component } from 'react'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
